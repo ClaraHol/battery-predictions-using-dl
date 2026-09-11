2f7bb9d4f524f2f10931b521d00de054fc5143dc
Clara Hollenbeck <s214722@dtu.dk>
2026-09-11 12:50:47 +0200

    Data processing files

--- project/src/data/Parquet_analysis.py
+++ project/src/data/Parquet_analysis.py
@@ -2,25 +2,29 @@ import pandas as pd
 import pyarrow.parquet as pq
 import matplotlib.pyplot as plt
 import numpy as np
+from pathlib import Path
 
 # Load the parquet file (here is an example of PD/cell 113)
-loadpath=r"C:\Users\Anton Espholm\Downloads\raw_data\raw_data\PD\113_single_parquet_with_soc_and_corrections.parquet"
+SCRATCH = Path("/work3/claho")
+DEST_ROOT = SCRATCH / "battery_datasets/processed"
+
+loadpath  =DEST_ROOT/"efc50_curves.parquet"
 
 # Load the quantities you'd like to use
 data=pq.read_table(loadpath).to_pandas()
 print(data.columns.tolist())
 
-current = np.array(data["current (A)"])
-voltage = np.array(data["voltage (V)"])
-step = np.array(data["step_no"])
-rpt = np.array(data["rpt"])
-soc = np.array(data["soc"])
+current = np.array(data["current_A"])
+voltage = np.array(data["voltage_V"])
+step = np.array(data["cycle_number"])
+rpt = np.array(data["temp_C"])
+soc = np.array(data["cell_id"])
 
 # You can also save the data as other format, like .csv
 # savepath="/save_path/"
 # data.to_csv(savepath+'XXX.csv',sep=',', index=False, mode='w', encoding='utf-8')
-
+print(step)
 # You can also visualize them
 plt.plot(voltage, linewidth=1.2)
 plt.plot(current, linewidth=1.2)
-plt.show()
+plt.savefig("test_parquet")
